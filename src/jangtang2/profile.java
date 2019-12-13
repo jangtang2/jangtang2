3475d82073e473c47d4d5e063fb0a2b2cb8a0f48
jangtang2 <58350257+jangtang2@users.noreply.github.com>
2019-12-13 22:00:54 +0900

    Update profile.java

--- src/jangtang2/profile.java
+++ src/jangtang2/profile.java
@@ -12,28 +12,49 @@ import jangtang2.MyFrame;
 
 public class profile {
 	private String name;
-	   private String address;
-	   private String phoneNumber;
-	   public profile (String name, String address, String phoneNumber) {
+	   private String realname;
+	   private int age;
+	   private int hight;
+	   private String group;
+	   private String comp;
+	   private int year;
+	   public profile (String name, String realname, int age, int hight, String group, String comp, int year) {
 	      this.name=name;
-	      this.address=address;
-	      this.phoneNumber=phoneNumber;
+	      this.realname=realname;
+	      this.age=age;
+	      this.hight=hight;
+	      this.group=group;
+	      this.comp=comp;
+	      this.year= year;
 	   }
 	   public String getname() {
 		      return name;
 		   }
-	   public String getaddress() {
-	      return address;
+	   public String getrealname() {
+	      return realname;
 	   }
-	   public String getphoneNumber() {
-	      return phoneNumber;
+	   public int getage() {
+	      return age;
 	   }
+	   public int gethight() {
+		      return hight;		
+		   }
+	   public String getgroup() {
+		      return group;
+		   }
+	   public String getcomp() {
+		      return comp;
+		      }
+	   public int getyear() {
+		      return year;
+		   }
 	}
 	 
 	class MyFrame extends JFrame{
 	   Container contentPane = getContentPane();
 	   JTextArea scrollpane = new JTextArea();
 	   JPanel menu = new JPanel();
+	   
 	   JButton search_1 = new JButton("검색");
 	   JButton view_1 = new JButton("모두 조회");
 	   JButton exit_1 = new JButton("종료");
@@ -44,14 +65,12 @@ public class profile {
 	   
 	   JPanel txt = new JPanel();
 	   JTextField nametxt = new JTextField("");
-	   JTextField phonetxt = new JTextField("");
-	   JTextField addrtxt = new JTextField("");
 	   
 	   HashMap<String, profile> pb =new HashMap<String, profile>();
 	   ArrayList<String> delname = new ArrayList<String>();
 	   
 	   public MyFrame() {
-	      setTitle("주소록");
+	      setTitle("예명가수 프로필");
 	      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	     
 	    
@@ -103,30 +122,31 @@ public class profile {
 	   private class MyActionListener implements ActionListener{
 	      public void actionPerformed(ActionEvent e) {
 	         JButton b = (JButton)e.getSource();
-	         int count=0;
+	 
 
-	          pb.put("김현정", new profile("김현정", "010123456", "주소 소주"));
-	          pb.put("김지연", new profile("김지연", "010123456", "주소 소주"));
-	          pb.put("임다영", new profile("임다영", "010123456", "주소 소주"));
-	             
+	          pb.put("설아", new profile("설아", "김현정", 26, 165, "우주소녀", "starship", 2016));
+	          pb.put("보나", new profile("보나", "김지연", 25, 163, "우주소녀", "starship", 2016));
+	          pb.put("은서", new profile("은서", "손주연", 22, 170, "우주소녀", "starship", 2016));
+	          pb.put("신비", new profile("신비", "황은비", 22, 166, "여자친구", "쏘스뮤직", 2015));
+	          pb.put("은하", new profile("은하", "정은비", 23, 163, "여자친구", "쏘스뮤직", 2015));
+	          pb.put("소원", new profile("소원", "김소정", 25, 173, "여자친구", "쏘스뮤직", 2015));
+	          pb.put("아이유", new profile("아이유", "이지은", 27, 161, "없음", "카카오M", 2008));
 	         
 	             
 	         
 	         
 	         if(b.getText().equals("검색")){
 	            scrollpane.setText("");
-	            Set<String> Keys = pb.keySet();
-	             Iterator<String> it = Keys.iterator();
-	             int n = pb.size();
+	                  
+	             
 	             
 	             profile searchname = pb.get(nametxt.getText());
 	            
 	                	   
 	                	  
-	             scrollpane.append("이름: "+ searchname.getname()+ "\n주소: "+searchname.getaddress()+"\n전화번호: "+searchname.getphoneNumber()+"\n");
+	             scrollpane.append("이름: "+ searchname.getname()+ "\n본명: "+searchname.getrealname()+"\n나이: "+searchname.getage()+"\n키: "+searchname.gethight()+"\n그룹: "+searchname.getgroup()+"\n소속사: "+searchname.getcomp()+"\n데뷔: "+searchname.getyear()+"\n");
 	                   
 	                
-	             
 	             
 	             nametxt.setText("");
 	            
@@ -137,12 +157,12 @@ public class profile {
 	            
 	            Set<String> Keys = pb.keySet();
 	            Iterator<String> it = Keys.iterator();
-	            int n = pb.size();
+	           
 	                          
 	            while(it.hasNext()){
 	               String name = it.next();
 	               profile searchname = pb.get(name);
-	               scrollpane.append("이름: " + searchname.getname()+"\n주소: "+searchname.getaddress()+"\n전화번호: "+searchname.getphoneNumber()+"\n");
+	               scrollpane.append(searchname.getname()+"\n");
 	               }
 	            
 	            nametxt.setText("");
